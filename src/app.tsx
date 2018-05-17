4b8afe7e0a1f1ddfbe9c8bc068a7565b595644b8
Olga Fandikova <olafan92@gmail.com>
2018-05-17 12:37:10 +0700

    Change 404 page

--- src/app.tsx
+++ src/app.tsx
@@ -14,7 +14,7 @@ class Content extends React.Component<{}, {}> {
 
     render() {
         return (
-            <Router basename={process.env.PUBLIC_URL}>
+            <Router>
                 <div>
                     <Switch>
                         <Route exact path="/" component={Main} />
